e21f358378f5f5bbe270ef235bdd0355cd2aa920
pullself <sckswf@hotmail.com>
2018-12-26 22:05:41 +0800

    mininet实验二py定义拓扑

---
+++ mininet实验二/single.py
@@ -0,0 +1,7 @@
+from mininet.net import Mininet
+from mininet.topo import SingleSwitchTopo
+Single3 = SingleSwitchTopo(k=3)   
+net = Mininet(topo=Single3)
+net.start()
+net.pingAll()
+net.stop()
---
+++ mininet实验二/tree.py
@@ -0,0 +1,7 @@
+from mininet.net import Mininet
+from mininet.topolib import TreeTopo
+Tree22 = TreeTopo(depth=2,fanout=2)
+net = Mininet(topo=Tree22)
+net.start()
+net.pingAll()
+net.stop()
