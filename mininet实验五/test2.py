0d3459dd5a0018f4e9003c484f559d6ca66aff63
pullself <sckswf@hotmail.com>
2018-12-29 11:39:30 +0800

    两交换机连接分别下挂三个主机拓扑

---
+++ mininet实验五/test2.py
@@ -0,0 +1,45 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+
+class SingleSwitchTopo(Topo):
+    def __init__(self, n=2, **opts):
+        Topo.__init__(self, **opts)
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch("s2")
+        self.addLink(switch1, switch2, bw=10, delay='5ms',
+                     loss=0, max_queue_size=1000, use_htb=True)
+        for h in range(n/2):
+            host = self.addHost('h%s' % (h + 1), cpu=.5/n)
+            self.addLink(host, switch1, bw=10, delay='5ms', loss=0,
+                         max_queue_size=1000, use_htb=True)
+        for h in range(n/2, n):
+            host = self.addHost('h%s' % (h + 1), cpu=.5/n)
+            self.addLink(host, switch2, bw=10, delay='5ms', loss=0,
+                         max_queue_size=1000, use_htb=True)
+
+
+def perfTest():
+    topo = SingleSwitchTopo(n=6)
+    net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    print "Testing bandwidth between h1 and h3"
+    h1, h3 = net.get('h1', 'h3')
+    net.iperf((h1, h3))
+    print "Testing bandwidth between h4 and h6"
+    h4, h6 = net.get('h4', 'h6')
+    net.iperf((h4, h6))
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    perfTest()
